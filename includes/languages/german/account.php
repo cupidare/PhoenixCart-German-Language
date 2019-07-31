56fc5fd6e3fc9a6f5fcce3d4a316865c8b102d4f
cupidare <jcupidare@gmail.com>
2019-07-31 11:07:18 +0200

    Create account.php

---
+++ includes/languages/german/account.php
@@ -0,0 +1,29 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2019 osCommerce CE Phoenix
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Konto');
+
+define('OVERVIEW_TITLE', 'Übersicht');
+define('OVERVIEW_SHOW_ALL_ORDERS', '(alle Bestellungen)');
+define('OVERVIEW_PREVIOUS_ORDERS', 'Bisherige Bestellungen');
+
+define('MY_ACCOUNT_TITLE', 'Konto');
+define('MY_ACCOUNT_INFORMATION', 'Kontoinformationen');
+define('MY_ACCOUNT_ADDRESS_BOOK', 'Adressbuch');
+define('MY_ACCOUNT_PASSWORD', 'Passwort ändern');
+
+define('MY_ORDERS_TITLE', 'Bestellungen');
+define('MY_ORDERS_VIEW', 'bisherige Bestellungen');
+
+define('EMAIL_NOTIFICATIONS_TITLE', 'E-Mail Benachrichtigungen');
+define('EMAIL_NOTIFICATIONS_NEWSLETTERS', 'Newsletter');
+define('EMAIL_NOTIFICATIONS_PRODUCTS', 'Benachrichtigungen');
