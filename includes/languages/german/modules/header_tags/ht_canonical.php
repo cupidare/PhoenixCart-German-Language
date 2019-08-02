9bcddc2e254823047a6e6881315bf79356f17272
cupidare <jcupidare@gmail.com>
2019-08-02 08:47:05 +0200

    Add files via upload

---
+++ includes/languages/german/modules/header_tags/ht_canonical.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2019 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_HEADER_TAGS_CANONICAL_TITLE', 'Kanonische Links');
+  define('MODULE_HEADER_TAGS_CANONICAL_DESCRIPTION', 'Fügt kanonische Linkvorschläge den Kateorie/Produktseiten hinzu. <div class="secWarning">hreflang-Attribut wird nicht unterstützt. Bitte bei mehrsprachigen Shops beachten.</div>'); 
