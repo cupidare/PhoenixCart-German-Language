713e42239267e84dd85eaa2afd7020780687bc0d
cupidare <jcupidare@gmail.com>
2019-09-05 12:50:12 +0200

    1.0.2.3

--- includes/languages/german/modules/boxes/bm_product_notifications.php
+++ includes/languages/german/modules/boxes/bm_product_notifications.php
@@ -15,3 +15,5 @@
   define('MODULE_BOXES_PRODUCT_NOTIFICATIONS_BOX_TITLE', 'Benachrichtigung');
   define('MODULE_BOXES_PRODUCT_NOTIFICATIONS_BOX_NOTIFY', 'Eine Nachricht bei änderungen zu <strong>%s</strong> versenden.');
   define('MODULE_BOXES_PRODUCT_NOTIFICATIONS_BOX_NOTIFY_REMOVE', 'Keine Nachricht bei änderungen zu <strong>%s</strong> versenden.');
+  
+  define('MODULE_BOXES_PRODUCT_NOTIFICATIONS_VIEW', '<i class="fas fa-eye"></i> zeige alle');
