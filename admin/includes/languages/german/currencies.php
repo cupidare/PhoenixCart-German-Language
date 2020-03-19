053aa8afeeb77271601f40b07d6de7fa0be979e2
cupidare <jcupidare@gmail.com>
2020-03-19 06:21:02 +0100

    1.0.5.5

--- admin/includes/languages/german/currencies.php
+++ admin/includes/languages/german/currencies.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2019 osCommerce CE Phoenix
+  Copyright (c) 2020 osCommerce
 
   Released under the GNU General Public License
 */
@@ -19,16 +19,16 @@ define('TABLE_HEADING_ACTION', 'Aktion');
 
 define('TEXT_INFO_EDIT_INTRO', 'Bitte führen Sie alle notwendigen Änderungen durch');
 define('TEXT_INFO_COMMON_CURRENCIES', '-- Verbreitete Währungen--');
-define('TEXT_INFO_CURRENCY_TITLE', 'Name:');
-define('TEXT_INFO_CURRENCY_CODE', 'Kürzel:');
-define('TEXT_INFO_CURRENCY_SYMBOL_LEFT', 'Symbol Links:');
-define('TEXT_INFO_CURRENCY_SYMBOL_RIGHT', 'Symbol Rechts:');
-define('TEXT_INFO_CURRENCY_DECIMAL_POINT', 'Dezimalkomma:');
-define('TEXT_INFO_CURRENCY_THOUSANDS_POINT', 'Tausenderpunkt:');
-define('TEXT_INFO_CURRENCY_DECIMAL_PLACES', 'Dezimalstellen:');
-define('TEXT_INFO_CURRENCY_LAST_UPDATED', 'letzte Änderung:');
-define('TEXT_INFO_CURRENCY_VALUE', 'Wert:');
-define('TEXT_INFO_CURRENCY_EXAMPLE', 'Beispiel:');
+define('TEXT_INFO_CURRENCY_TITLE', 'Name: %s');
+define('TEXT_INFO_CURRENCY_CODE', 'Kürzel: %s');
+define('TEXT_INFO_CURRENCY_SYMBOL_LEFT', 'Symbol Links: %s');
+define('TEXT_INFO_CURRENCY_SYMBOL_RIGHT', 'Symbol Rechts: %s');
+define('TEXT_INFO_CURRENCY_DECIMAL_POINT', 'Dezimalkomma: %s');
+define('TEXT_INFO_CURRENCY_THOUSANDS_POINT', 'Tausenderpunkt: %s');
+define('TEXT_INFO_CURRENCY_DECIMAL_PLACES', 'Dezimalstellen: %s');
+define('TEXT_INFO_CURRENCY_LAST_UPDATED', 'letzte Änderung: %s');
+define('TEXT_INFO_CURRENCY_VALUE', 'Wert: %s');
+define('TEXT_INFO_CURRENCY_EXAMPLE', 'Beispiel: %s= %s');
 define('TEXT_INFO_INSERT_INTRO', 'Bitte geben Sie die neue Währung mit allen relevanten Daten ein');
 define('TEXT_INFO_DELETE_INTRO', 'Sind Sie sicher, dass Sie diese Währung löschen möchten?');
 define('TEXT_INFO_HEADING_NEW_CURRENCY', 'neue Währung');
@@ -40,3 +40,4 @@ define('TEXT_INFO_CURRENCY_UPDATED', 'Die Verbrauchssteuer für %s (%s) wurde er
 define('ERROR_REMOVE_DEFAULT_CURRENCY', 'Fehler: Die Standardwährung darf nicht gelöscht werden. Bitte definieren Sie eine neue Standardwährung und wiederholen Sie den Vorgang.');
 define('ERROR_CURRENCY_INVALID', 'Error: Die Verbrauchssteuer für %s (%s) wurde nicht aktualisiert via %s. Ist es ein gültiger Währungcode?');
 define('WARNING_PRIMARY_SERVER_FAILED', 'Warnung: Der primäre Server (%s) scheiterte bei %s (%s) - teste sekundären Server.');
+define('ERROR_INSTALL_CURRENCY_CONVERTER', 'Derzeit ist kein Währungsumrechner installiert.  <a class="alert-link font-weight-bold" href="' . tep_href_link('modules.php', 'set=currencies') . '">jetzt installieren</a>');
--- admin/includes/languages/german/mail.php
+++ admin/includes/languages/german/mail.php
@@ -5,21 +5,22 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2019 osCommerce
+  Copyright (c) 2020 osCommerce
 
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'eMail an Kunden versenden');
+const HEADING_TITLE = 'eMail an Kunden versenden';
+ 
+const TEXT_CUSTOMER = 'Kunde';
+const TEXT_SUBJECT = 'Betreff';
+const TEXT_FROM = 'Absender';
+const TEXT_FROM_ADDRESS = 'Von-Adresse';
+const TEXT_MESSAGE = 'Nachricht';
 
-define('TEXT_CUSTOMER', 'Kunde');
-define('TEXT_SUBJECT', 'Betreff');
-define('TEXT_FROM', 'Absender');
-define('TEXT_MESSAGE', 'Nachricht');
+const TEXT_SELECT_CUSTOMER = 'Kunden auswählen';
+const TEXT_ALL_CUSTOMERS = 'Alle Kunden';
+const TEXT_NEWSLETTER_CUSTOMERS = 'An alle Newsletter-Abonnenten';
 
-define('TEXT_SELECT_CUSTOMER', 'Kunden auswählen');
-define('TEXT_ALL_CUSTOMERS', 'Alle Kunden');
-define('TEXT_NEWSLETTER_CUSTOMERS', 'An alle Newsletter-Abonnenten');
-
-define('NOTICE_EMAIL_SENT_TO', 'Hinweis: eMail wurde versendet an: %s');
-define('ERROR_NO_CUSTOMER_SELECTED', 'Fehler: Es wurde kein Kunde ausgewählt.');
+const NOTICE_EMAIL_SENT_TO = 'Hinweis: eMail wurde versendet an: %s';
+const ERROR_NO_CUSTOMER_SELECTED = 'Fehler: Es wurde kein Kunde ausgewählt.';
