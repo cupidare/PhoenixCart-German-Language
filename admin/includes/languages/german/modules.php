fdeef4ba62cc3f23deed3e58a922f7734eda34bd
cupidare <jcupidare@gmail.com>
2020-05-07 06:16:07 +0200

    1.0.6.0

--- admin/includes/languages/german/modules.php
+++ admin/includes/languages/german/modules.php
@@ -11,15 +11,15 @@
 */
 
 const TABLE_HEADING_MODULES=  'Module';
-const TABLE_HEADING_SORT_ORDER=  'Reihenfolge';
-const TABLE_HEADING_ENABLED=  'Aktiv';
+const TABLE_HEADING_SORT_ORDER=  'Sortierreihenfolge';
 const TABLE_HEADING_ACTION=  'Aktion';
 
 const TEXT_INFO_VERSION = 'Version:';
 const TEXT_INFO_ONLINE_STATUS=  'online-Status';
-const TEXT_INFO_API_VERSION = 'API Version:';
+const TEXT_INFO_API_VERSION = 'API-Version:';
 
 const TEXT_MODULE_DIRECTORY=  'Modul-Verzeichnis:';
 
 const ERROR_MODULE_UNMET_REQUIREMENT = 'Bei diesem Modul fehlt eine Abhängigkeit.';
 const ERROR_MODULE_HAS_DEPENDENTS = 'Andere Module benötigen dieses Modul.';
+
