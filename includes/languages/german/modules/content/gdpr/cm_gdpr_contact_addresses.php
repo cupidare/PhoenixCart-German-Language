b325abb29e10829efe86e92fcc9d5495f4d41a04
cupidare <jcupidare@gmail.com>
2020-05-07 10:18:27 +0200

    Update cm_gdpr_contact_addresses.php

--- includes/languages/german/modules/content/gdpr/cm_gdpr_contact_addresses.php
+++ includes/languages/german/modules/content/gdpr/cm_gdpr_contact_addresses.php
@@ -15,5 +15,5 @@
   
   const MODULE_CONTENT_GDPR_CONTACT_ADDRESSES_PUBLIC_TITLE = 'Weitere angegebenen Adressen';
   
-  const MODULE_CONTENT_GDPR_CONTACT_ADDRESSES_NUM_ADDRESSES = 'Es wurde(n) <br><span class="h1"><span class="num_addresses">%s</span></span><br> weitere Addrese(n) gefunden.'';
+  const MODULE_CONTENT_GDPR_CONTACT_ADDRESSES_NUM_ADDRESSES = 'Es wurde(n) <br><span class="h1"><span class="num_addresses">%s</span></span><br> weitere Addrese(n) gefunden.';
   
