50ff9bb42994404bc335f0c7dbd629414c403018
cupidare <jcupidare@gmail.com>
2020-07-15 08:53:46 +0200

    Update ext_directory_listing.php

--- admin/includes/languages/german/modules/security_check/extended/ext_directory_listing.php
+++ admin/includes/languages/german/modules/security_check/extended/ext_directory_listing.php
@@ -10,6 +10,5 @@
   Released under the GNU General Public License
 */
 
-define('MODULE_SECURITY_CHECK_EXTENDED_EXT_DIRECTORY_LISTING_TITLE', 'ext/ Directory Listing');
-define('MODULE_SECURITY_CHECK_EXTENDED_EXT_DIRECTORY_LISTING_HTTP_200', 'The <a href="' . tep_catalog_href_link('ext/') . '" target="_blank">' . DIR_WS_CATALOG . 'ext/</a> directory is publicly accessible and/or browsable - please disable directory listing for this directory in your web server configuration.');
-
+define('MODULE_SECURITY_CHECK_EXTENDED_EXT_DIRECTORY_LISTING_TITLE', 'ext/ Verzeichnis-Ansicht');
+define('MODULE_SECURITY_CHECK_EXTENDED_EXT_DIRECTORY_LISTING_HTTP_200', 'Das <a href="' . tep_catalog_href_link('ext/') . '" target="_blank">' . DIR_WS_CATALOG . 'ext/</a>-Verzeichnis ist öffentlich zugänglich. Bitte deaktivieren Sie den Verzeichnis-Zugriff für dieses Verzeichnis in Ihrer Webserver-Konfiguration');
