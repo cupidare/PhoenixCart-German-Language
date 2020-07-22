854c36673e248889664eb1c392d1e70eba0864e3
cupidare <jcupidare@gmail.com>
2020-07-22 08:58:51 +0200

    Update cd_street_address.php

--- includes/languages/german/modules/customer_data/cd_street_address.php
+++ includes/languages/german/modules/customer_data/cd_street_address.php
@@ -10,9 +10,9 @@
   Released under the GNU General Public License
 */
 
-const MODULE_CUSTOMER_DATA_STREET_ADDRESS_TEXT_TITLE = 'Straßenname';
+const MODULE_CUSTOMER_DATA_STREET_ADDRESS_TEXT_TITLE = 'Straße/Haus-Nr.';
 const MODULE_CUSTOMER_DATA_STREET_ADDRESS_TEXT_DESCRIPTION = 'Zeigt einen Eintrag für den Straßenname während der Registrierung';
 
-const ENTRY_STREET_ADDRESS = 'Straßenname';
+const ENTRY_STREET_ADDRESS = 'Straße/Haus-Nr.';
 const ENTRY_STREET_ADDRESS_ERROR = 'der Straßenname muss mindestens %d Zeichen aufweisen';
 const ENTRY_STREET_ADDRESS_TEXT = '';
