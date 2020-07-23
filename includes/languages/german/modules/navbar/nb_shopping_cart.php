f8b94e9e45b2028da50703122fef5da9f5bdd7dc
cupidare <jcupidare@gmail.com>
2020-07-23 07:34:36 +0200

    Update nb_shopping_cart.php

--- includes/languages/german/modules/navbar/nb_shopping_cart.php
+++ includes/languages/german/modules/navbar/nb_shopping_cart.php
@@ -19,7 +19,7 @@
   
   define('MODULE_NAVBAR_SHOPPING_CART_CONTENTS', '<i title="Warenkorb" class="fas fa-shopping-cart fa-fw"></i><span class="d-inline d-sm-none d-md-inline"> <span class="cart-count">%1$s</span> Artikel</span> <span class="caret"></span>');
   define('MODULE_NAVBAR_SHOPPING_CART_NO_CONTENTS', '<i class="fas fa-shopping-cart fa-fw"></i> 0 Artikel');
-  define('MODULE_NAVBAR_SHOPPING_CART_HAS_CONTENTS', '<span class="cart-count">%s</span> Artikel, <span class="cart-value">%s</span>');
+  define('MODULE_NAVBAR_SHOPPING_CART_HAS_CONTENTS', '<span class="cart-count">%s</span> Artikel: <span class="cart-value">%s</span>');
   define('MODULE_NAVBAR_SHOPPING_CART_VIEW_CART', 'zeige Warenkorb');
   define('MODULE_NAVBAR_SHOPPING_CART_CHECKOUT', '<i class="fas fa-angle-right fa-fw"></i> Kasse');
   
