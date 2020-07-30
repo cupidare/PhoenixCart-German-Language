9d3ecc46668fdb077d8345d3f82ed9b43159a0b1
cupidare <jcupidare@gmail.com>
2020-07-30 06:44:50 +0200

    1.0.7.6

---
+++ admin/includes/languages/german/info_pages.php
@@ -0,0 +1,50 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+const HEADING_TITLE = 'Page Manager';
+
+const TABLE_HEADING_PAGE_ID = 'ID';
+const TABLE_HEADING_PAGE_TITLE = 'Seitenüberschrift';
+const TABLE_HEADING_SLUG = 'URL-Name';
+const TABLE_HEADING_DATE_ADDED = 'hinzugefügt am';
+const TABLE_HEADING_ACTION = 'Aktion';
+const TABLE_HEADING_STATUS = 'Status';
+
+const ENTRY_FROM_NICKNAME = 'Name';
+const ENTRY_DATE = 'Datum';
+
+
+const TEXT_INFO_DATE_ADDED = 'hinzugefügt am: %s';
+const TEXT_INFO_LAST_MODIFIED = 'zuletzt geändert: %s';
+
+const TEXT_INFO_PAGE_SIZE = 'Größe: %s Wörter';
+
+const TEXT_PAGE_STATUS = 'Status';
+const TEXT_PAGE_PUBLISHED = 'veröffentlicht';
+const TEXT_PAGE_NOT_PUBLISHED = 'nicht veröffentlicht';
+
+const PAGE_TITLE = 'Überschrift';
+const PAGE_TEXT = 'Seitentext';
+const PAGE_SLUG = 'URL-Name';
+const NAVBAR_TITLE = 'Navbar Überschrift';
+
+const TEXT_HEADING_DELETE_PAGE = 'Lösche Seite: %s';
+const TEXT_DELETE_PAGE_INTRO = 'Möchten Sie diese Seite löschen?';
+
+
+const TEXT_DISPLAY_NUMBER_OF_PAGES = 'zeige <b>%s</b> bis <b>%s</b> (von <b>%s</b> Seiten)';
+
+const IMAGE_BUTTON_ADD_PAGE = 'Neue Seite hinzufügen';
+const SORT_ORDER = 'Reihenfolge';
+const TABLE_HEADING_SORT_ORDER = 'Reihenfolge';
+
+const MISSING_SLUGS_ERROR = 'URL-Name fehlt:  <b>%s</b><hr>URL-Namen werden benötigt, um verschiedene Funktionen zu ermöglichen.';
--- includes/languages/german/hooks/shop/siteWide/MATC.php
+++ includes/languages/german/hooks/shop/siteWide/MATC.php
@@ -14,11 +14,5 @@
 const ENTRY_MATC = 'Einverständniserklärung';
 const ENTRY_MATC_TEXT = 'Klicken Sie auf die Box zum Akzeptieren des Widerrufrechts und der <a href="#" role="button" class="card-link ml-0 border-bottom border-primary" data-toggle="modal" data-target="#TCModal">Allgemeinen Geschäftsbedingungen</a> sowie der <a href="#" role="button" class="card-link ml-0 border-bottom border-primary" data-toggle="modal" data-target="#PModal">Datenschutzerklärung</a>.'; 
 
-// for the modal popups
+// for the modal popup
 const MATC_BUTTON_CLOSE = 'Schließen';
-
-const MATC_PRIVACY_MODAL_TITLE = 'Datenschutzerklärung';
-const MATC_PRIVACY_MODAL_TEXT = '<p>Put here your Privacy Text...</p>';
-
-const MATC_TERMS_MODAL_TITLE = 'AGB';
-const MATC_TERMS_MODAL_TEXT = '<p>Put here your Terms & Conditions Text...</p>';
