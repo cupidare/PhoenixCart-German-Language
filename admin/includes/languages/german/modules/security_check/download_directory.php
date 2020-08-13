c85b173a16d302c2b26931d1d189c7b82c6efa0b
cupidare <jcupidare@gmail.com>
2020-08-13 21:21:11 +0200

    Add files via upload

--- admin/includes/languages/german/modules/security_check/download_directory.php
+++ admin/includes/languages/german/modules/security_check/download_directory.php
@@ -5,10 +5,9 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2019 osCommerce CE Phoenix
+  Copyright (c) 2020 osCommerce
 
   Released under the GNU General Public License
 */
 
-define('WARNING_DOWNLOAD_DIRECTORY_NON_EXISTENT', 'Es existiert kein Verzeichnis für Artikel zum Herunterladen: ' . DIR_FS_DOWNLOAD . '. Man kann keine Produkte herunterladen, solange ein solches Verzeichnis nicht existiert.');
-
+const WARNING_DOWNLOAD_DIRECTORY_NON_EXISTENT = 'Es existiert kein Verzeichnis für Artikel zum Herunterladen:  [%s]. Man kann keine Produkte herunterladen, solange ein solches Verzeichnis nicht existiert.';
