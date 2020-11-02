bbb985a7e13579e1c197609a88e5fa8346781e30
cupidare <jcupidare@gmail.com>
2020-11-02 10:55:39 +0100

    1.0.7.10

---
+++ includes/languages/german/modules/notifications/n_update_order.php
@@ -0,0 +1,30 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+const MODULE_NOTIFICATIONS_UPDATE_ORDER_TEXT_TITLE = 'Status der Bestellung aktualisieren';
+const MODULE_NOTIFICATIONS_UPDATE_ORDER_TEXT_DESCRIPTION = 'Eine Nachricht schicken, wenn der Status aktualisiert wird.';
+
+const MODULE_NOTIFICATIONS_UPDATE_ORDER_SEPARATOR = '------------------------------------------------------';
+const MODULE_NOTIFICATIONS_UPDATE_ORDER_TEXT_SUBJECT = 'Bestellung aktualisieren';
+const MODULE_NOTIFICATIONS_UPDATE_ORDER_TEXT_ORDER_NUMBER = 'Bestellnummer:  %d';
+const MODULE_NOTIFICATIONS_UPDATE_ORDER_TEXT_INVOICE_URL = 'Ausführliche Rechnug:  %s';
+const MODULE_NOTIFICATIONS_UPDATE_ORDER_TEXT_DATE_ORDERED = 'Bestelldatum:  %s';
+const MODULE_NOTIFICATIONS_UPDATE_ORDER_TEXT_STATUS_UPDATE = <<<'EOT'
+Ihre Bestellung wurde aktualisiert.
+
+Neuer Status: %s
+
+Bitte antworten Sie auf diese Mail, wenn Sie Fragen haben.
+
+
+EOT;
+const MODULE_NOTIFICATIONS_UPDATE_ORDER_TEXT_COMMENTS_UPDATE = 'Kommentare für Ihre Bestellung sind ' . "\n\n%s\n\n";
