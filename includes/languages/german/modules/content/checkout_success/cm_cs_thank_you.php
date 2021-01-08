ddeaac62776c91fbd955b309fc39666e133c4bb4
cupidare <jcupidare@gmail.com>
2021-01-08 12:46:08 +0100

    Update cm_cs_thank_you.php

--- includes/languages/german/modules/content/checkout_success/cm_cs_thank_you.php
+++ includes/languages/german/modules/content/checkout_success/cm_cs_thank_you.php
@@ -15,7 +15,7 @@
 
   define('MODULE_CONTENT_CHECKOUT_SUCCESS_TEXT_THANKS_FOR_SHOPPING', '<strong>Vielen Dank für Ihren Einkauf!</strong>');
   define('MODULE_CONTENT_CHECKOUT_SUCCESS_TEXT_SUCCESS', '<p class="font-weight-lighter mb-0"><b>Ihr Bestellung ist ausgeführt worden.</b></p>');
-  define('MODULE_CONTENT_CHECKOUT_SUCCESS_TEXT_SEE_ORDERS', '<a class="list-group-item list-group-item-action" href="%s"><span class="text-primary border-bottom border-primary"> Bestellstatus </span> hier können Sie jederzeit in Ihrem Kundenkonto unter einsehen.</a>');
-  define('MODULE_CONTENT_CHECKOUT_SUCCESS_TEXT_CONTACT_STORE_OWNER', '<a class="list-group-item list-group-item-action" href="%s"><span class="text-primary border-bottom border-primary">Kontaktformular</span> stellen Sie Fragen zu Ihrer Bestellung.</a>');
+  define('MODULE_CONTENT_CHECKOUT_SUCCESS_TEXT_SEE_ORDERS', '<a class="list-group-item list-group-item-action" href="%s"><span class="text-primary border-bottom border-primary">Bestellstatus:</span> verfolgen Sie den Status Ihre Bestellung</a>');
+  define('MODULE_CONTENT_CHECKOUT_SUCCESS_TEXT_CONTACT_STORE_OWNER', '<a class="list-group-item list-group-item-action" href="%s"><span class="text-primary border-bottom border-primary">Kontaktformular:</span> stellen Sie Fragen zu Ihrer Bestellung</a>');
   
   
