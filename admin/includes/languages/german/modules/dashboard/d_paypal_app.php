f8dd6afbf214ac4d3d175eba3fece44b3079240d
cupidare <jcupidare@gmail.com>
2021-03-08 08:42:09 +0100

    1.0.8.0

--- admin/includes/languages/german/modules/dashboard/d_paypal_app.php
+++ admin/includes/languages/german/modules/dashboard/d_paypal_app.php
@@ -2,14 +2,14 @@
 /*
   $Id$
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
 
-  Copyright (c) 2019 osCommerce CE Phoenix
+  Copyright (c) 2021 Phoenix Cart
 
   Released under the GNU General Public License
 */
 
 // language definitions have been moved to:
 // catalog/includes/apps/paypal/languages/english/admin/modules/dashboard/d_paypal_app.php
-
+?>
