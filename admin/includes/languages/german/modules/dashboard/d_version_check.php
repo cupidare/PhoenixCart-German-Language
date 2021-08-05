c5f2538caab24f236018428548d7ba8e5ad3b886
cupidare <jcupidare@gmail.com>
2021-08-05 22:00:49 +0200

    Update d_version_check.php

--- admin/includes/languages/german/modules/dashboard/d_version_check.php
+++ admin/includes/languages/german/modules/dashboard/d_version_check.php
@@ -17,3 +17,11 @@ define('MODULE_ADMIN_DASHBOARD_VERSION_CHECK_CHECK_NOW', 'Prüfe jetzt');
 define('MODULE_ADMIN_DASHBOARD_VERSION_CHECK_NEVER', 'Nie');
 
 define('MODULE_ADMIN_DASHBOARD_VERSION_CHECK_UPDATE_AVAILABLE', '<i class="fas fa-exclamation-circle"></i> Es ist ein Update für OSCOM CE Phoenix verfügbar!');
+
+const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_TITLE = 'Versions-Check';
+const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_DESCRIPTION = 'Zeige Ergebnis des Versions-Checks';
+const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_DATE = 'Zuletzt geprüft';
+const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_CHECK_NOW = 'Prüfe jetzt';
+
+const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_UPDATE_AVAILABLE = '<i class="fas fa-exclamation-circle"></i> Es ist ein Update für OSCOM CE Phoenix verfügbar!';
+const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_IS_LATEST = '<i class="fas fa-thumbs-up"></i> Dies ist die aktuellste Version von CE Phoenix!';
