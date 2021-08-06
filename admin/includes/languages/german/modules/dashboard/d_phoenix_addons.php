17e5c74b6ff609af077fb2cc674bf175e76466d0
cupidare <jcupidare@gmail.com>
2021-08-06 22:41:17 +0200

    1.0.8.5

--- admin/includes/languages/german/modules/dashboard/d_phoenix_addons.php
+++ admin/includes/languages/german/modules/dashboard/d_phoenix_addons.php
@@ -10,11 +10,7 @@
   Released under the GNU General Public License
 */
 
-define('MODULE_ADMIN_DASHBOARD_PHOENIX_ADDONS_TITLE', 'ZERTIFIZIERTE Add-Ons & Dienste');
-define('MODULE_ADMIN_DASHBOARD_PHOENIX_ADDONS_DESCRIPTION', 'Zeige die neuesten Add-Ons für Phoenix-Club-Mitglieder');
+const MODULE_ADMIN_DASHBOARD_PHOENIX_ADDONS_TITLE = 'geprüfte Add-Ons & Dienste';
+const MODULE_ADMIN_DASHBOARD_PHOENIX_ADDONS_DESCRIPTION = 'Zeige die neuesten Add-Ons für Phoenix-Club-Mitglieder';
 
-define('MODULE_ADMIN_DASHBOARD_PHOENIX_ADDONS_OWNER', 'von');
-define('MODULE_ADMIN_DASHBOARD_PHOENIX_ADDONS_RATING', 'Bewertung');
-
-define('MODULE_ADMIN_DASHBOARD_PHOENIX_JOIN_CLUB', 'Club beitreten');
-define('MODULE_ADMIN_DASHBOARD_PHOENIX_VIEW_ALL', 'zeige gesamte Liste');
+const MODULE_ADMIN_DASHBOARD_PHOENIX_VIEW_ALL = 'zeige gesamte Liste';
